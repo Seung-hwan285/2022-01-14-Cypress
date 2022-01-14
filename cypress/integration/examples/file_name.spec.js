6f33e6956145fb52104c40363437db2cbc542bf6
Seung-hwan285 <ghks285@naver.com>
2022-01-14 15:40:44 +0900

    feat : plus 버튼 max값 12

--- cypress/integration/examples/file_name.spec.js
+++ cypress/integration/examples/file_name.spec.js
@@ -2,7 +2,7 @@
 - [x] 생성시 버튼과 초기값(10)을 렌더링 한다.
 - [x] + 버튼을 클릭 시 count가 1증가한다.
 - [x] - 버튼을 클릭 시 count가 1감소한다.
-- [ ] + 버튼을 눌렀을 때 count가 12가 넘는 경우 더이상 증가하지 못한다. (Max 값이 12)
+- [x] + 버튼을 눌렀을 때 count가 12가 넘는 경우 더이상 증가하지 못한다. (Max 값이 12)
 - [ ] - 버튼을 눌렀을 때 count는 8보다 작아지는 경우 감소하지 못한다. (Min 값이 8)
 
  */
@@ -35,6 +35,19 @@ describe("container",()=>{
 
            cy.get(".minus-button").click();
            cy.get(".count-display").should("have.value",inputValue-1);
+       });
+    });
+
+    it("+ max값 12",()=>{
+       cy.get(".count-display").then(input=>{
+           const inputValue = Number(input.val());
+
+           cy.get(".plus-button").click();
+           cy.get(".plus-button").click();
+           cy.get(".plus-button").click();
+
+           cy.get(".count-display").should("have.value",12);
+
        });
     });
 });
